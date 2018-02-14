6fcdaa936434b7544eebcaedb4e39b9dc2f02c71
Bob Zhang <zzt1981@126.com>
2018-02-14 11:13:03 +0800

    create

--- my-generator-spring-data/src/main/java/org/catcafe/generator/SourceGenerator.java
+++ my-generator-spring-data/src/main/java/org/catcafe/generator/SourceGenerator.java
@@ -1,9 +1,13 @@
 package org.catcafe.generator;
 
+import static org.catcafe.util.FreeMarkerUtils.
+import java.util.HashMap;
+import java.util.Map;
+
 public class SourceGenerator {
 
 	public void generateEntity() {
-		
+		Map<String, Object> model = new HashMap<>();
 	}
 	
 	public void generateRepository() {
--- my-generator-spring-data/src/test/java/org/catcafe/ApplicationTests.java
+++ my-generator-spring-data/src/test/java/org/catcafe/ApplicationTests.java
@@ -1,7 +1,15 @@
 package org.catcafe;
 
+import java.sql.SQLException;
+
+import org.catcafe.util.DBInfoUtil;
+import org.catcafe.util.DBInfoUtil.TableInfo;
+import org.catcafe.util.JsonUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +17,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
+	
+	@Autowired
+	DBInfoUtil dbInfoUtil;
+	
 	@Test
 	public void contextLoads() {
+		try {
+			TableInfo tableInfo = dbInfoUtil.getTableInfo("user");
+			logger.debug(JsonUtil.obj2json(tableInfo));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
