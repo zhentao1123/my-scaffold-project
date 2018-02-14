d1076b5fdaa113a8b1f5101d1cf35134ac988edf
Bob Zhang <zzt1981@126.com>
2018-02-14 11:14:40 +0800

    create

--- my-generator-spring-data/src/main/java/org/catcafe/generator/SourceGenerator.java
+++ my-generator-spring-data/src/main/java/org/catcafe/generator/SourceGenerator.java
@@ -1,6 +1,6 @@
 package org.catcafe.generator;
 
-import static org.catcafe.util.FreeMarkerUtils.
+import static org.catcafe.util.FreeMarkerUtils.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +8,7 @@ public class SourceGenerator {
 
 	public void generateEntity() {
 		Map<String, Object> model = new HashMap<>();
+		generateFileByFile(templateFilePath, destFilePath, configuration, model);
 	}
 	
 	public void generateRepository() {
